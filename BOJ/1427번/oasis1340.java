088a42472f02b7524a1a99d52c02a01a992c4718
장재영 <oasis1340@gmail.com>
2025-10-08 00:05:56 +0900

    Create oasis1340.java

---
+++ BOJ/1427번/oasis1340.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.next();
+        
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < n.length(); i++) {
+            String[] num = (n.split(""));
+            nums.add(Integer.parseInt(num[i]));
+        }
+
+        Collections.sort(nums);
+        Collections.reverse(nums);
+        
+        for (int i = 0; i < nums.size(); i++) {
+        	System.out.print(nums.get(i));
+        }
+    }
+}
