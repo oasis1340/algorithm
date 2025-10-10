a3314977fe90bd3777b60789774a965485819141
장재영 <oasis1340@gmail.com>
2025-10-11 00:36:13 +0900

    Create oasis1340.java

---
+++ BOJ/5086번/oasis1340.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+                
+        String factor = "factor";
+        String multiple = "multiple";
+        String neither = "neither";
+        
+        ArrayList<String> result = new ArrayList<String>();
+        
+		while ( true ) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if (a != 0 && b != 0) {
+				if ( a >= b && a % b == 0 ) {
+					result.add(multiple);
+				} else if ( a < b && b % a == 0 ) {
+					result.add(factor);
+				} else if ( a % b != 0 || b % a != 0 ) {
+					result.add(neither);
+				}
+			}
+			if ( a == 0 && b == 0) {
+				break;
+			}
+		}
+        
+        for (int i = 0; i < result.size(); i++) {
+        	System.out.println(result.get(i));
+        }
+    }
+}
